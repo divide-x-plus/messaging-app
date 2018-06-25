2ee2d1d55af340c799632e8f715964b60bb03a5a
Jim Z. Li <zl238@cornell.edu>
2018-06-25 15:39:25 -0700

    basic socket setup

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
+const public_path = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 var app = express();
+// createServer takes a function with (req, res) arguments
+var server = http.createServer(app);
+// instantiate a web socket server
+var io = socketIO(server);
 
-let public_path = path.join(__dirname, '../public');
+
 
 app.use(express.static(public_path));
 
-app.get('/', (req, res) => {
-  res.send('index.html');
+// register an event listener to a web socket
+io.on('connection', (socket) => {
+  console.log('A new user is connected');
+
+  socket.on('disconnect', () => {
+    console.log('server is disconnected')
+  })
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is up on ${port}`);
 });
