7cd92ae4d0c56410cd561999b228311d7489949d
Jim Z. Li <zl238@cornell.edu>
2018-06-28 16:42:42 -0700

    added acknowledgements

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,23 @@
 var socket = io();
 
+// handling connection related events
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  // socket.emit('createMessage', {
-  //   from: 'zl238@cornell.edu',
-  //   text: 'responding my previous email to Z'
-  // })
 })
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
 })
 
+// handling message events
 socket.on('newMessage', (message) => {
   console.log('new message has been received', message);
+})
+
+// example of acknowledgements
+socket.emit('createMessage', {
+  from: 'Timea',
+  text: "Let's go to Ferris Wheel"
+}, (data) => {
+  console.log('An acknowledgement has been received: ', data);
 })
--- server/server.js
+++ server/server.js
@@ -13,34 +13,27 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 
-
 app.use(express.static(public_path));
+
 
 // register an event listener to a web socket
 io.on('connection', (socket) => {
   console.log('A new user is connected');
 
+  socket.on('disconnect', () => { console.log('server is disconnected')})
+
+  // new user events
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chat App!'))
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'))
 
-  socket.on('createMessage', (message) => {
-    console.log("A message has been created", message);
-
-    // io.emit() emits an event to every connection
-    // socket.emit() emits to one single connection
-
+  // new message events
+  socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
-
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
-  })
-
-  socket.on('disconnect', () => {
-    console.log('server is disconnected')
+    callback({
+      ackTime: new Date().getTime(),
+      transmission: true
+    })
   })
 })
 
